bb18c5001449b65d28a498f592db8a04f45b06e7
rahul meewa <themeewa@gmail.com>
2022-05-18 01:19:39 +0530

    added rec curve and real-vs-pred graph

--- src/evaluate.py
+++ src/evaluate.py
@@ -17,11 +17,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import dataframe_image as dfi
 
-from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_absolute_error, r2_score
 from sklearn.pipeline import Pipeline
 from preprocess_n_tune import root_mean_squared_error, read_data
 from docopt import docopt
-from slickml.metrics import RegressionMetrics
+from scipy.integrate import simps
+# from slickml.metrics import RegressionMetrics
 
 def load_model(path_prefix):
     """
@@ -42,7 +43,7 @@ def load_model(path_prefix):
         
     return model
 
-def evaluate_model(model, algo, X_test, y_test):
+def evaluate_model(model, algo, X_test, y_test, path_prefix):
     """
     Evaluates the best fit model on the test data
 
@@ -68,8 +69,10 @@ def evaluate_model(model, algo, X_test, y_test):
     test_scores[f"Optimized_MAE"] = mean_absolute_error(y_test, predictions)
     test_scores[f"Optimized_RMSE"] = root_mean_squared_error(y_test, predictions)
 #     REC CURVE
-    reg_metrics = RegressionMetrics(y_test, predictions)
-    reg_metrics.plot()
+#     reg_metrics = RegressionMetrics(y_test, predictions)
+#     reg_metrics.plot()
+    plot_REC(y_test, predictions, path_prefix)
+    plot_comparision(y_test, predictions, path_prefix)
 
     return predictions, pd.DataFrame(test_scores, index=["Test Score"])
 
@@ -115,13 +118,69 @@ def plot_predictions(y_true, y_pred, path_prefix):
     plt.ylabel("Log Predicted Area(ha)")
     plt.savefig(f"{path_prefix}_{config.result_prefix}_predictions.png")
 
+# Function for Regression Error Characteritic Curve
+
+def REC(y_true , y_pred):
+
+    # initilizing the lists
+    Accuracy = []
+
+    # initializing the values for Epsilon
+    Begin_Range = 0
+    End_Range = 1.5
+    Interval_Size = 0.01
+
+    # List of epsilons
+    Epsilon = np.arange(Begin_Range , End_Range , Interval_Size)
+
+    # Main Loops
+    for i in range(len(Epsilon)):
+        count = 0.0
+        for j in range(len(y_true)):
+            if np.linalg.norm(y_true[j] - y_pred[j]) / np.sqrt( np.linalg.norm(y_true[j]) **2 + np.linalg.norm(y_pred[j])**2 ) < Epsilon[i]:
+                count = count + 1
+
+        Accuracy.append(count/len(y_true))
+
+    # Calculating Area Under Curve using Simpson's rule
+    AUC = simps(Accuracy , Epsilon ) / End_Range
+
+    # returning epsilon , accuracy , area under curve
+    return Epsilon , Accuracy , AUC
+#
+
+def plot_comparision(y_true , y_pred, path_prefix):
+    RR = r2_score(y_true , y_pred)
+    xx = np.arange(1, y_pred.size + 1, 1)
+    plt.figure(figsize=(6 , 6))
+    plt.scatter(xx, y_true, linewidths=.5, marker ="s", label="real")
+    plt.scatter(xx, y_pred, linewidths=.5, alpha = 0.5, label="predicted")
+    plt.xlabel("Measured")
+    plt.ylabel("Predicted")
+#     plt.plot([y_true.min(), y_true.max()], [xx.min(), xx.max()], 'k--', lw=1)
+    plt.text(45, -5, r"$R^2 = %0.4f$" %RR , fontsize=15)
+    plt.legend(loc='upper center', shadow=True, fontsize='small')
+    plt.savefig(f"{path_prefix}_{config.result_prefix}_predictions_comparison.png")
+    plt.show()
+
+
+def plot_REC(y_true , y_pred, path_prefix):
+    Deviation , Accuracy, AUC = REC(y_true , y_pred)
+    plt.figure(figsize=(6 , 6))
+    plt.title("Regression Error Characteristic (REC)")
+    plt.plot(Deviation, Accuracy, "--b",lw =1)
+    plt.xlabel("Deviation")
+    plt.ylabel("Accuracy (%)")
+    plt.text(1.1, 0.07, "AUC = %0.4f" %AUC , fontsize=15)
+    plt.savefig(f"{path_prefix}_{config.result_prefix}_REC.png")
+#     plt.show()
 
 def main(opt):
     algo = opt["--algo"]
     model = load_model(opt["--results_path"])
     X_test, y_test = read_data(opt["--test_data"])
     
-    predictions, results = evaluate_model(model,algo, X_test, y_test)
+    predictions, results = evaluate_model(model,algo, X_test, y_test, opt["--results_path"])
     assert(isinstance(results, pd.DataFrame))
 
     store_results(results, opt["--results_path"])
